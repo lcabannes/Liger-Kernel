cd270da858bdebccc21fe784da54b2c2f27ea66f
lcabannes <loic@DESKTOP-9N93DRN>
2025-02-19 16:32:04 +0100

    working bechmark with torch and direct triton kernel

--- test_ce_loss.py
+++ test_ce_loss.py
@@ -1,4 +1,5 @@
 from liger_kernel.transformers import LigerFusedLinearCrossEntropyLoss
+from src.liger_kernel.ops.fused_linear_cross_entropy import fused_linear_cross_entropy_forward
 import torch.nn as nn
 import torch
 import torch.nn.functional as F
@@ -6,16 +7,18 @@ import pytest
 import time
 import timeit
 
+
 torch.manual_seed(0)
 vocab_size = 4096
 hidden_dim = 128
 model = nn.Linear(hidden_dim, vocab_size).cuda()
 
 # fuses linear + cross entropy layers together and performs chunk-by-chunk computation to reduce memory
-loss_fn = LigerFusedLinearCrossEntropyLoss()
+loss_fn = fused_linear_cross_entropy_forward # LigerFusedLinearCrossEntropyLoss()
 
 
 context_size = 4 * 4096
+print(f"starting benchmark with context_size {context_size} ")
 input = torch.randn(context_size, hidden_dim, requires_grad=True, device="cuda")
 target = torch.randint(vocab_size, (context_size, ), device="cuda")
 
@@ -25,7 +28,7 @@ def torch_ce(model, inputs, targets):
     return loss
 
 torch_loss = torch_ce(model, input, target)
-liger_loss = loss_fn(model.weight, input, target)
+liger_loss, z_loss = loss_fn(input, model.weight, target)
 print(f"torch loss: {torch_loss.item()}")
 print(f"liger loss: {liger_loss.item()}")
 
